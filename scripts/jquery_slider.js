fbe9f31f50d9bc2d236115915ced1cd1cfe3df3a
victoria <v.suprun1961@gmail.com>
2025-10-09 07:55:03 +0300

    fix: fix styles

--- scripts/jquery_slider.js
+++ scripts/jquery_slider.js
@@ -6,8 +6,77 @@ $(document).ready(function(){
     infinite: true,
     speed: 300,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    // autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arroys: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arroys: true
+        }
+      }
+    ]
   });
 });
 
 
+
+// ////////////////////////////////////////////////////
+$(document).ready(function(){
+  $('.slider').slick({
+    arroys: true,
+    dots: true,
+    adapriveHeight: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 1000,
+    easing: 'linear',
+    infinite: true,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    pauseOnFocus: false,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    draggable: true,
+    swipe: true,
+    touchThreshold: 5,
+    touchMove: true,
+    waitForAnimate: true,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arroys: true
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arroys: true
+        }
+      }
+    ]
+  });
+});
